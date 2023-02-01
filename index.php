acc0454c620e74cc8c2f62836ebff14a48226469
DgrinderHZ <hassanzekkouri@gmail.com>
2023-02-01 17:33:00 +0100

    initial commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/materialize.min.css">
+    <title>Ecole | Home</title>
+</head>
+<body class="purple darken-3 white-text">
+    hi ecole
+</body>
+</html>
