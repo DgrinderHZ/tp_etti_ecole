d3cb50031e9e743a3ba09abd58c08d173ed77232
DgrinderHZ <hassanzekkouri@gmail.com>
2023-02-05 08:06:07 +0100

    ajouter le formulaire

--- modifier_profile.php
+++ modifier_profile.php
@@ -1,8 +1,29 @@
 <?php require_once("templates/header.php"); ?>
-<?php require_once("templates/main_nav.php"); 
+<?php require_once("templates/main_nav.php");?>
 
-?>
-
-Modification de profile
+<section>
+    <h1>Modifier votre informations du profile</h1>
+    <form action="">
+        <div>
+            <label for="nom">Nom</label>
+            <input type="text" id="nom" name="nom">
+        </div>
+        <div>
+            <label for="prenom">prenom</label>
+            <input type="text" id="prenom" name="prenom">
+        </div>
+        <div>
+            <label for="email">Email</label>
+            <input type="text"id="email" name="email">
+        </div>
+        <div>
+            <label for="tel">N° tele</label>
+            <input type="text" id="tel" name="tel">
+        </div>
+        <div>
+            <input type="submit" value="Modifier">
+        </div>
+    </form>
+</section>
     
 <?php require_once("templates/footer.php"); ?>
